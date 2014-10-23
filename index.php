3daa2b722023c39136b20164107e5b629780e8de
Aleksandr2015 <charleksvik@gmail.com>
2014-10-23 15:02:46 +0300

    старт формления index.php

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
         <meta name="description" content="<?php bloginfo('description');?>">    
         <title><?php the_title(); ?> | <?php bloginfo('name');?></title>
         <link rel="shortcut icon" href="">
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/bootstrap.css">
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/bootstrap-theme.css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/style.css">
 
         <!--[if IE]>
             <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -20,20 +19,24 @@
     <body>
             
         <header class="blog-header">
-        <!-- Navigation primary / Start -->
-        <?php include(TEMPLATEPATH . '/navigation-primary.php'); ?>
-        <!-- Navigation primary / End -->
+            <div class="container">
+               <div class="header-inner-wrapper">
+                    <!-- Navigation primary / Start -->
+                    <?php include(TEMPLATEPATH . '/navigation-primary.php'); ?>
+                    <!-- Navigation primary / End -->
+                </div><!-- .header-inner-wrapper / end -->
+            </div><!-- .container / end -->
     
         
-        </header>
+        </header><!-- .blog-header / end -->
     
         <section class="blog-main">
             
-        </section>
+        </section><!-- .blog-main / end -->
         
         <footer class="blog-footer">
             
-        </footer>
+        </footer><!-- .blog-footer / end -->
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="<?php echo get_template_directory_uri();?>/js/bootstrap.js"></script>        
