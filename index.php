c39049335638477e3ffea7e73e8d5c344c06c7dc
Aleksandr2015 <charleksvik@gmail.com>
2014-10-30 16:26:52 +0200

    добавил яндекс ментрики счетчик

--- index.php
+++ index.php
@@ -7,11 +7,12 @@
         <title><?php the_title(); ?> | <?php bloginfo('name');?></title>
         <link rel="shortcut icon" href="">
         <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/style.css">
-
         <!--[if IE]>
             <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
             <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
+        <!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter26866323 = new Ya.Metrika({id:26866323, webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/26866323" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
+        <!-- yandex-verification --><meta name='yandex-verification' content='551483b0e34cbea2' /><!-- yandex-verification / end-->
         <?php if ( is_user_logged_in() ) {echo '<style>.navbar.navbar-inverse.navbar-fixed-top {margin-top:32px;}</style>';} ?>
         <?php wp_head(); ?>  
     </head>
