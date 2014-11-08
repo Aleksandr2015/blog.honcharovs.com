aac237fa6ca0a6477f8320ec6ba08ac3cf09dbd9
Aleksandr2015 <charleksvik@gmail.com>
2014-11-08 13:53:31 +0200

    вывел сайдбар через get_sidebar

--- index.php
+++ index.php
@@ -45,18 +45,7 @@
                     </div>
                 </div>
                 <!-- Aside content -->
-                <div class="col-xs-4">
-                    <aside>
-                        <section>
-                            <div class="right-menu">
-                                <h2 class="title">Рубрики</h2>
-                                <ul>
-                                    <li><a href="">WordPress</a></li>
-                                </ul>
-                            </div>
-                        </section>                        
-                    </aside>                    
-                </div>
+                <?php get_sidebar();?>
             </div>
             
         </section><!-- .blog-main / end -->
---
+++ sidebar.php
@@ -0,0 +1,12 @@
+                <div class="col-xs-4">                    
+                    <aside>
+                        <section>
+                            <div class="right-menu">
+                                <h2 class="title">Рубрики</h2>
+                                <ul>
+                                    <li><a href="">WordPress</a></li>
+                                </ul>
+                            </div>
+                        </section>                        
+                    </aside>                    
+                </div>
